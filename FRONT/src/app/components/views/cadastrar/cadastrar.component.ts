cb1ba728e553b34bb9d7554dda8f21e6fd8a9a6e
Rafael Ovidio Leal <rafaelovidioleal@gmail.com>
2021-09-27 19:30:01 -0300

    front

---
+++ FRONT/src/app/components/views/cadastrar/cadastrar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar',
+  template: `
+    <p>
+      cadastrar works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CadastrarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ FRONT/src/app/services/livro.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Livro } from "../models/livro";
+
+@Injectable({
+    providedIn: "root",
+})
+export class LivroService {
+    private baseURL = "http://localhost:5000/api/livro";
+
+    constructor(private http: HttpClient) {}
+
+    list(): Observable<Livro[]> {
+        return this.http.get<Livro[]>(`${this.baseURL}/list`);
+    }
+
+    getById(Id: number): Observable<Livro> {
+        return this.http.get<Livro>(`${this.baseURL}/getbyId/${Id}`);
+    }
+
+    create(livro: Livro): Observable<Livro> {
+        return this.http.post<Livro>(`${this.baseURL}/create`, livro);
+    }
+}
