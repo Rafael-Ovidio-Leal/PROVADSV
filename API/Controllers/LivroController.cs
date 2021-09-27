a71afb92fb5cce84d1a5c01044d162efba238318
Rafael Ovidio Leal <rafaelovidioleal@gmail.com>
2021-09-27 19:07:49 -0300

    create

---
+++ API/Controllers/LivroController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using API.Data;
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/livro")]
+    public class LivroController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+         public LivroController(DataContext context){
+            _context = context;
+        }
+
+        // POST: api/livro/criar
+        [HttpPost]
+        [Route("criar")]
+        public livro create(Livro livro)
+        {
+            _context.livros.Add(livro);
+            _context.SaveChanges();
+            return livro;
+        }
+
+        // GET: api/livro/listar
+        [HttpGet]
+        [Route("listar")]
+        public async Task<IActionResult> ListAsync() => Ok(await _context.livros.ToListAsync());
+
+       // GET: api/livro/listarId/
+        [HttpGet]
+        [Route("listarId/{Id}")]
+        public async Task<IActionResult> GetByIdAsync([FromRoute] int id)
+        {
+            livro livro = await _context.livros.FindAsync(id);
+            if (livro != null)
+            {
+                return Ok(livro);
+            }
+            return NotFound();
+        }
+    }
+}
