5bcced483bf94a090a8191617042e4bad1bfbede
Rafael Ovidio Leal <rafaelovidioleal@gmail.com>
2021-09-27 20:13:13 -0300

    a

---
+++ FRONT/src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { ListarComponent } from './components/views/listar/listar.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HeaderComponent } from './components/template/header/header.component';
+import { CadastrarComponent } from './components/views/cadastrar/cadastrar.component';
+//Jogo
+
+const routes: Routes = [
+    //USUARIO
+    {
+      path: "",
+      component: HeaderComponent,
+    },
+    {
+      path: "livro/cadastrar",
+      component: CadastrarComponent,
+    },
+    {
+        path: "livro/listar",
+        component: ListarComponent,
+    },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ FRONT/src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSelectModule } from '@angular/material/select';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CadastrarComponent } from './components/views/cadastrar/cadastrar.component';
+import { ListarComponent } from './components/views/listar/listar.component';
+import { ContentComponent } from './components/template/content/content.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './components/template/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CadastrarComponent,
+    ContentComponent,
+    ListarComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatSnackBarModule,
+    MatSelectModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
